c119311b0f1730c432118b1ae13f7c501419717e
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-02 13:03:35 +0300

    Add mongoose connection

--- server.js
+++ server.js
@@ -8,10 +8,22 @@ const corsOptions = {
 };
 
 const app = express();
-
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require('./app/models');
+
+db.mongoose
+  .connect(db.url, {
+  })
+  .then(() => {
+    console.log('Connected to the database.');
+  })
+  .catch((err) => {
+    console.log('Cannot connect to the database.', err);
+    process.exit();
+  });
 
 app.get('/', (req, res) => {
   res.json({ message: 'Request accepted.' });
