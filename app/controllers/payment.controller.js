bacac7b2005ba6e923bfea87d70e404be3c89827
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-02 13:19:41 +0300

    Define payment controller

---
+++ app/controllers/payment.controller.js
@@ -0,0 +1,10 @@
+const db = require('../models/index');
+
+const Payment = db.payments;
+
+exports.create = (req, res) => {};
+exports.findAll = (req, res) => {};
+exports.findOne = (req, res) => {};
+exports.update = (req, res) => {};
+exports.delete = (req, res) => {};
+exports.deleteAll = (req, res) => {};
--- app/models/payment.model.js
+++ app/models/payment.model.js
@@ -1,25 +1,28 @@
-module.exports = (mongoose) => {
-  const paymentSchema = new mongoose.Schema(
-    {
-      cardNumber: {
-        type: Number,
-        required: true,
-      },
-      expDate: {
-        type: String,
-        required: true,
-      },
-      cvv: {
-        type: Number,
-        required: true,
-      },
-      amount: {
-        type: Number,
-        required: true,
-      },
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const paymentSchema = new Schema(
+  {
+    cardNumber: {
+      type: Number,
+      required: true,
     },
-    { timestamps: true },
-  );
-  const Payment = mongoose.model('payment', paymentSchema);
-  return Payment;
-};
+    expDate: {
+      type: String,
+      required: true,
+    },
+    cvv: {
+      type: Number,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Payment = mongoose.model('payment', paymentSchema);
+module.exports = Payment;
